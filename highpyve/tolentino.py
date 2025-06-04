7322ff8bc24301db93230c37ddfab6ca9ee3a3a6
Anipan_SigmundKristoffer <krisanipan2205@gmail.com>
2025-06-04 21:46:27 +0800

    Refactor comments in tolentino.py

--- highpyve/tolentino.py
+++ highpyve/tolentino.py
@@ -48,9 +48,7 @@ def tolentino_menu():
                 print("We do create our opportunities. Love the quote!")
                 buffer()
             case 5:
-                clear_screen()
-                print("Keep up the good work, this turned out great!")
-                buffer()
+                show_anipan_comment()
             case 6:
                 clear_screen()
                 print("You handle challenges with such calm and focus.")
@@ -91,4 +89,11 @@ def show_quotes():
 
 def exit_program():
     clear_screen()
+    buffer()
+
+def show_anipan_comment():
+    clear_screen()
+    print("Comment from Highpyve-Kristoffer")
+    clear_screen()
+    print("Keep up the good work, this turned out great!")
     buffer()
